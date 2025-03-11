5c8290b04c37cdf22f5d4df4a47fe5fd158d3e61
Tanner Kirkendall <tkirkendall@trshealthcare.com>
2025-03-11 16:46:02 -0500

    vh

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -9,12 +9,24 @@ function Login(){
         loadLogin();
       }, []);
 
+    const handleSubmit = async (e) => {
+          e.preventDefault()   
+          try {
+                  var utcStartTime = luxon.DateTime.fromISO(startTime)
+                  var utcEndTime = luxon.DateTime.fromISO(endTime)
+                  var a = await patchActivities(utcStartTime, utcEndTime, description, activity.id);
+              } catch (err) {
+              console.log(err)
+              } finally {
+                  onClose()
+              }
+        }
     return (
     
     <div className="login">
         <h2>You must login</h2>
 
-        
+
     </div>
     );
 }
